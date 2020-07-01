235345bb6e0aa349cde4a3d11179a160cc599bf6
Lewis Lin <lewislin@yahoo.com>
2020-07-01 17:50:26 -0400

    Handle tie using MusicalXML data.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from operator import itemgetter, attrgetter
 
 #0 define standard note class
 class Note:
-	def __init__(self,m,x,p,t,d,s):
+	def __init__(self,m,x,p,t,d,s,tie):
 		self.measure=int(m)
 		self.default_x=int(x)
 		self.weight_x=0
@@ -13,6 +13,7 @@ class Note:
 		self.type=t
 		self.duration=int(d) if d != '' else 0
 		self.staff=int(s)
+		self.tie=tie.strip()
 		self.connect2=-1
 		
 		# becase the pitch name with # or b is different from played note, translate it to be the same
@@ -43,8 +44,9 @@ class Note_p:
 
 
 #2 read standard notes from file
-f=open('1_Bach_WellTemperedClavier_Prelude_No.5_BWV846_Cmajor_全曲.csv')
+#f=open('1_Bach_WellTemperedClavier_Prelude_No.5_BWV846_Cmajor_全曲.csv')
 #f=open('2_Bach_WellTemperedClavier_Prelude_No.5_BWV850_Dmajor_全曲.csv')
+f=open('3_Mozart_PianoSonata_No.11_Amajor_K.331W_6variations_不反.csv')
 datalist = []
 i=0
 for line in f:
@@ -57,8 +59,9 @@ for line in f:
 	datalist.append(n)
 
 #3 read played notes from file 
-f=open('pio01_Bach1_S1_T1.csv')
+#f=open('pio01_Bach1_S1_T1.csv')
 #f=open('pio01_Bach2_S1_T1.csv')
+f=open('pio01_Mozart1_S1_T1.csv')
 datalist_p = []
 i=0
 for line in f:
@@ -123,18 +126,18 @@ def compareNote(n1,n2):
 		return False        
 		#return False
 
-def getPrevNode(current, hand):
+def getPrevNote(current, hand):
 	for i in range(current-1,-1,-1):
 		if datalist[i].staff == hand:
 			return datalist[i]
 	return None
 
-def findTie(index, node, prev_node):
-	tie = getTieList(index, node.staff)
-	if node.pitch in tie:
-		print("skipping tie {} {} {}".format(i, node.pitch, node.staff))
-		node.connect2 = prev_node.connect2
-		datalist_m.append(datalist_p[node.connect2])
+def findTie(index, note, prev_note):
+	tie = getTieList(index, note.staff)
+	if note.pitch in tie:
+		print("skipping tie {} {} {}".format(i, note.pitch, note.staff))
+		note.connect2 = prev_note.connect2
+		datalist_m.append(datalist_p[note.connect2])
 		return True
 	return False
 
@@ -148,61 +151,75 @@ def getTieList(index, hand):
 		tie_list[key] = []
 	return tie_list[key]
 
-def addNode(index, node):
-	tie = getTieList(index, node.staff)
-	tie.append(node.pitch)
+def addNote(index, note):
+	tie = getTieList(index, note.staff)
+	tie.append(note.pitch)
 
-def switchTieList(node):
-	last_key = getTieKey("last", node.staff)
-	current_key = getTieKey("current", node.staff)
-	tie_list[last_key] = getTieList("current", node.staff)
+def switchTieList(note):
+	last_key = getTieKey("last", note.staff)
+	current_key = getTieKey("current", note.staff)
+	tie_list[last_key] = getTieList("current", note.staff)
 	tie_list[current_key] = []
-	tie_list[current_key].append(node.pitch)
+	tie_list[current_key].append(note.pitch)
 
+def getTieStart(current, stop_note):
+	for t in range(current-1,-1,-1):
+		note = datalist[t]
+		if note.staff == stop_note.staff and note.tie == 'start' and note.pitch == stop_note.pitch:
+			return datalist[t]
+	return None
 
 #8 start to comapre notes one on one
 datalist_m = []
+curOnsetTime = 0
 for i in range(len(datalist)):
-	node = datalist[i]
-	prev_node = getPrevNode(i, node.staff)
-	if prev_node != None:
-		if node.default_x == prev_node.default_x:
-			if findTie("current", node, prev_node):
-				continue
-			addNode("current", node)
-		else:
-			switchTieList(node)
-			if findTie("last", node, prev_node):
-				continue
+	note = datalist[i]
+	if note.tie == "stop":
+		prev_note = getTieStart(i, note)
+		print("skipping tie {} {} {} {}".format(i, note.pitch, note.staff, datalist_p[prev_note.connect2].onsettime))
+		note.connect2 = prev_note.connect2
+		datalist_m.append(datalist_p[note.connect2-1])
+		continue
+	# prev_note = getPrevNote(i, note.staff)
+	# if prev_note != None:
+	# 	if note.default_x == prev_note.default_x:
+	# 		if findTie("current", note, prev_note):
+	# 			continue
+	# 		addNote("current", note)
+	# 	else:
+	# 		switchTieList(note)
+	# 		if findTie("last", note, prev_note):
+	# 			continue
 
 	for j in range(len(datalist_p)):
 		# check if the note has been connected to 
 		if datalist[i].connect2 == -1 and datalist_p[j].connect2 == -1:
 			# compare the played note if we can find the same pitch within 0.3 seconds onset time.
-			curOnsetTime = datalist_p[datalist[i-1].connect2 -1].onsettime if i >0 else 0
-			print("{}({}) {} {} {}".format(i, node.measure, node.pitch, node.staff, curOnsetTime))
+			#curOnsetTime = datalist_p[datalist[i-1].connect2 -1].onsettime if i >0 else 0
+			print("{}({}) {} {} {} {}".format(i, note.measure, note.pitch, note.staff, curOnsetTime, note.tie))
 			k=j
 			isFound=False
-			while k<len(datalist_p) and (curOnsetTime == 0 or datalist_p[k].onsettime-curOnsetTime < 1.5 ):
+			while k<len(datalist_p) and (curOnsetTime == 0 or datalist_p[k].onsettime-curOnsetTime < 2 ):
             # Bach1 = 0.3, Bach 2 = 0.3,
-				node_p = datalist_p[k]
+				note_p = datalist_p[k]
 				not_checked = datalist_p[k].connect2 == -1
 				not_too_old = datalist_p[k].onsettime-curOnsetTime > -1 
 				if not_checked and not_too_old:
-					print("  checking {}({}) {}".format(node_p.pitch, k, node_p.onsettime))
+					print("  checking {}({}) {}".format(note_p.pitch, k, note_p.onsettime))
 				if not_checked and not_too_old and compareNote(datalist[i],datalist_p[k])==True:
 					print("   Found!")
 					# we found the same note. Add it to the datalist_m
 					datalist[i].connect2=k+1
 					datalist_p[k].connect2=i+1
 					datalist_m.append(datalist_p[k])
+					curOnsetTime = datalist_p[k].onsettime
 					isFound=True
 					break
 				else:
 					k+=1				
 			
 			if isFound==False:
-				print("   note out of range {}", datalist_p[k].onsettime)
+				print("   note out of range {}", datalist_p[k-1].onsettime)
 				# we cannot find the same note. Create a patch note and add to the datalist_m
 				n = Note_p(datalist[i].pitch,0,0,0,0)
 				datalist[i].connect2=datalist[i-1].connect2
@@ -214,10 +231,10 @@ for i in range(len(datalist)):
 			
 #9 write the result to a csv file
 f = open("compare.csv", "w")
-f.write('Standard Notes,,,,,,,,,Played Notes\n')
-f.write('measure,default_x,weight_x,pitch,type,duration,staff,,,pitch,onset time,offset time, duration,velocity\n')
+f.write('Standard Notes,,,,,,,,,,Played Notes\n')
+f.write('measure,default_x,weight_x,pitch,type,duration,staff,tie,,,pitch,onset time,offset time, duration,velocity\n')
 for i in range(len(datalist_m)):
-	f.write(f'{datalist[i].measure:1d},{datalist[i].default_x:3d},{datalist[i].weight_x:4d},{datalist[i].pitch:3s},{datalist[i].type:3s},{datalist[i].duration:3d},{datalist[i].staff:1d}')
+	f.write(f'{datalist[i].measure:1d},{datalist[i].default_x:3d},{datalist[i].weight_x:4d},{datalist[i].pitch:3s},{datalist[i].type:7s},{datalist[i].duration:3d},{datalist[i].staff:1d},{datalist[i].tie:5s}')
 	f.write(f',,,')
 	if datalist_m[i].duration==0:
 		f.write(f'{datalist_m[i].pitch:3s}(patch),{datalist_m[i].onsettime:8.3f},{datalist_m[i].offsettime:8.3f},{datalist_m[i].duration:8.3f},{datalist_m[i].velocity:8.3f},\n')
